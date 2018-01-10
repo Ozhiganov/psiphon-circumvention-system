de2b2e71ff3a2464876164967eef0d7e2d01e238
Adam Kruger <a.kruger@psiphon.ca>
2018-01-10 11:50:12 -0500

    Latest psiphond 26b3c8b

---
+++ Client/psiclient/htmldlg.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2012, Psiphon Inc.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Adapted from the MSDN htmldlg demo. Available here: http://www.microsoft.com/en-us/download/details.aspx?id=944
+
+#include "stdafx.h"
+#include "htmldlg.h"
+
+
+/**************************************************************************
+ResourceToUrl()
+**************************************************************************/
+tstring ResourceToUrl(LPCTSTR resourceName, LPCTSTR urlQuery, LPCTSTR urlFragment)
+{
+    tstring url;
+
+    url += _T("res://");
+
+    TCHAR   szTemp[MAX_PATH * 2];
+    GetModuleFileName(NULL, szTemp, ARRAYSIZE(szTemp));
+    url += szTemp;
+
+    url += _T("/");
+    url += resourceName;
+
+    // URI encoding seems to be taken care of automatically (fortuitous, but unsettling)
+
+    if (urlQuery)
+    {
+        url += _T("?");
+        url += urlQuery;
+    }
+
+    if (urlFragment)
+    {
+        url += _T("#");
+        url += urlFragment;
+    }
+
+    return url;
+}
